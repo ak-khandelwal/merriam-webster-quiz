aac190953510bf4f1c2cbdab4c1f211ecb7ef9e2
aryan khandelwal <aryankhandelwal15@gmail.com>
2025-08-13 18:37:23 +0530

    added word type in quiz

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -4,7 +4,10 @@ function Question({ question, dispatch, answer }) {
   return (
     <div className="question_container">
       <h4 className="my-7 mx-2 sm:mx-auto font-bold text-4xl sm:text-5xl">
-        {question.question}
+        {question.question}{' '}
+        <span class="text-amber-200">
+          {question.type ? `(${question.type})` : ''}
+        </span>
       </h4>
       <Options question={question} dispatch={dispatch} answer={answer} />
     </div>
